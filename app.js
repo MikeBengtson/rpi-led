232b68dd79951373b07fc6c73d19f79e598a1127
Mike Bengtson <mike.bengtson@gmail.com>
2016-11-20 11:13:53 +0000

    bug fixes before switching pattern

--- app.js
+++ app.js
@@ -8,5 +8,16 @@ app.use(bodyParser.urlencoded({ extended: true }));
 var routes = require("./routes/routes.js")(app);
  
 var server = app.listen(3000, function () {
-    console.log("Listening on port %s...", server.address().port);
+
+  app.gpio = require('rpi-gpio');
+
+  app.gpio.setup(8, app.gpio.DIR_OUT, function() {
+    app.gpio.setup(10, app.gpio.DIR_OUT, function() {
+      app.gpio.setup(16, app.gpio.DIR_OUT, function() {
+
+        console.log("Listening on port %s...", server.address().port);
+
+      });
+    });
+  });
 });
--- routes/routes.js
+++ routes/routes.js
@@ -1,5 +1,3 @@
-var gpio = require('rpi-gpio');
-
 var appRouter = function(app) {
   app.get("/status", function(req, res) {
       res.send("up");
@@ -8,17 +6,15 @@ var appRouter = function(app) {
   app.put("/led", function(req, res) {
     if(req.query.id && req.query.state) 
     { 
-      gpio.setup(req.query.id, gpio.DIR_OUT, function() {
-        gpio.write(req.query.id, req.query.state=="on", function(err) {
+        app.gpio.write(req.query.id, req.query.state=="on", function(err) {
           if (!err) {
-      			gpio.read(req.query.id, function(err, value) { 
+      			app.gpio.read(req.query.id, function(err, value) { 
 							res.send(value ? "on" : "off")
       			});
           } else {
              res.send("ERROR (led: " + req.query.id + ", state: " + req.query.state + "): " + err)
           };
         });
-      });
     } else
     {
       res.send("error: no led and/or state query param");
@@ -28,8 +24,8 @@ var appRouter = function(app) {
   app.get("/led", function(req, res) {
     if(req.query.id)
     { 
-      gpio.read(req.query.id, function(err, value) { 
-	res.send(value ? "on" : "off")
+      app.gpio.read(req.query.id, function(err, value) { 
+	      res.send(value ? "on" : "off")
       });
     } else
     {
